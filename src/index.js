b2324966a2956612abd1c61a03fc88ad8203d4f4
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-09-09 19:54:51 -0600

    Add SASS to the webpack setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./style.css";
+import "./style.scss";
 
 const text = document.querySelector("body");
 text.textContent = "Hello, world!";
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,54 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-
 module.exports = {
-    entry: {
-        index: './src/index.js',
-    },
-    mode: "development",
-    devtool: "inline-source-map",
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(?:js|mjs|cjs)$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: [
-                      ['@babel/preset-env', { targets: "defaults" }]
-                    ]
-                  }
-                }
-              }          
+  entry: {
+    index: "./src/index.js",
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Battleship',
-            favicon: "./src/favicon.png/",
-        }),
+      },
     ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Battleship",
+      favicon: "./src/favicon.png/",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
 };
