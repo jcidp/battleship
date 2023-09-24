edf3247b293ed5e7c21c4ae8b28d7cc10bbbeff3
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-09-24 13:19:49 -0600

    Place ships randomly in the boards

--- src/modules/game.js
+++ src/modules/game.js
@@ -9,12 +9,77 @@ const gameController = (() => {
   const getPlayer = () => player;
   const getComputer = () => computer;
 
+  const isCoordinateFree = (board, row, col) => {
+    if (board[row][col].ship) return false;
+    if (row > 0 && board[row - 1][col].ship) return false;
+    if (col < 9 && board[row][col + 1].ship) return false;
+    if (row < 9 && board[row + 1][col].ship) return false;
+    if (col > 0 && board[row][col - 1].ship) return false;
+    return true;
+  };
+
+  const isPositionValid = (board, start, end) => {
+    const [startCol, startRow] =
+      board.constructor.getIndexesFromCoordinates(start);
+    const [endCol, endRow] = board.constructor.getIndexesFromCoordinates(end);
+    const distance =
+      startRow === endRow ? endCol - startCol + 1 : endRow - startRow + 1;
+    for (let i = 0; i < distance; i++) {
+      if (startRow === endRow) {
+        if (!isCoordinateFree(board.board, startRow, startCol + i))
+          return false;
+      } else if (!isCoordinateFree(board.board, startRow + i, startCol)) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const getRandomShipPosition = (player, length) => {
+    let initialPosition;
+    let finalPosition;
+    let validPosition = false;
+    while (!validPosition) {
+      initialPosition = Player.getCoordinatesFromNumber(
+        Math.floor(Math.random() * 100) + 1,
+      );
+      const direction = Math.random() < 0.5 ? "horizontal" : "vertical";
+      if (direction === "horizontal") {
+        finalPosition =
+          String.fromCharCode(
+            initialPosition.charCodeAt(0) + length - 1 <= 74
+              ? initialPosition.charCodeAt(0) + length - 1
+              : initialPosition.charCodeAt(0) - length + 1,
+          ) + initialPosition.slice(1);
+      } else {
+        const initialNumber = +initialPosition.slice(1);
+        finalPosition =
+          initialPosition[0] +
+          (initialNumber + length - 1 <= 10
+            ? initialNumber + length - 1
+            : initialNumber - length + 1);
+      }
+      if (
+        initialPosition.charCodeAt(0) > finalPosition.charCodeAt(0) ||
+        +initialPosition.slice(1) > +finalPosition.slice(1)
+      ) {
+        [initialPosition, finalPosition] = [finalPosition, initialPosition];
+      }
+      validPosition = isPositionValid(
+        player.board,
+        initialPosition,
+        finalPosition,
+      );
+    }
+    return [initialPosition, finalPosition];
+  };
+
   const createPlayerShips = (player) => {
-    player.board.placeShip("F2", "J2");
-    player.board.placeShip("A4", "D4");
-    player.board.placeShip("D6", "F6");
-    player.board.placeShip("I4", "I6");
-    player.board.placeShip("F9", "G9");
+    player.board.placeShip(...getRandomShipPosition(player, 5));
+    player.board.placeShip(...getRandomShipPosition(player, 4));
+    player.board.placeShip(...getRandomShipPosition(player, 3));
+    player.board.placeShip(...getRandomShipPosition(player, 3));
+    player.board.placeShip(...getRandomShipPosition(player, 2));
   };
 
   const gameOver = (winner) => {
